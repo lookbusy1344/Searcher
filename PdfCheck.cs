1f5d32acba4b9b071c8d3407e50757c2ec7670e8
John <john@john.com>
2023-07-04 10:57:03 +0100

    Searching in PDFs inside ZIPs

--- PdfCheck.cs
+++ PdfCheck.cs
@@ -1,5 +1,6 @@
 ﻿using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
+using System.IO.Compression;
 using System.Text.RegularExpressions;
 
 namespace Searcher;
@@ -7,37 +8,59 @@ namespace Searcher;
 internal partial class PdfCheck
 {
 	/// <summary>
+	/// Check inside a PDF zip entry for a string
+	/// </summary>
+	public static bool CheckStream(ZipArchiveEntry entry, string content, StringComparison strcomp)
+	{
+		using var stream = entry.Open();
+		using var reader = new PdfReader(stream);
+
+		var result = SearchPdfInternal(reader, content, strcomp);
+
+		if (result == SearchResult.Error) throw new Exception("Error reading PDF file");
+		return result == SearchResult.Found;
+	}
+
+	/// <summary>
 	/// Search inside a PDF file for a string
 	/// </summary>
-	public static SearchResult CheckPdfForContent(string path, string content, StringComparison strcomp)
+	public static SearchResult CheckPdfFile(string path, string content, StringComparison strcomp)
 	{
-		var containsspace = content.Contains(' ', strcomp);
-
 		try
 		{
 			using var pdfReader = new PdfReader(path);
-			using var pdfDoc = new PdfDocument(pdfReader);
-
-			var pages = pdfDoc.GetNumberOfPages();
-			for (var i = 1; i <= pages; ++i)
-			{
-				// get the page text
-				var page = pdfDoc.GetPage(i);
-				var text = PdfTextExtractor.GetTextFromPage(page);
-
-				// if we are searching for a string with spaces, replace all whitespace with a single space
-				if (containsspace)
-					text = AnyNumberWhitespace().Replace(text, " ");
-
-				// does the page text contain our search string?
-				if (text.Contains(content, strcomp)) return SearchResult.Found;
-			}
-			return SearchResult.NotFound;
+			return SearchPdfInternal(pdfReader, content, strcomp);
 		}
 		catch
 		{
 			return SearchResult.Error;
 		}
+	}
+
+	/// <summary>
+	/// Internal helper to search inside a PdfReader object (file or stream) for a string
+	/// </summary>
+	private static SearchResult SearchPdfInternal(PdfReader reader, string content, StringComparison strcomp)
+	{
+		var containsspace = content.Contains(' ', strcomp);
+
+		using var pdfDoc = new PdfDocument(reader);
+		var pages = pdfDoc.GetNumberOfPages();
+
+		for (var i = 1; i <= pages; ++i)
+		{
+			// get the page text
+			var page = pdfDoc.GetPage(i);
+			var text = PdfTextExtractor.GetTextFromPage(page);
+
+			// if we are searching for a string with spaces, replace all whitespace with a single space
+			if (containsspace)
+				text = AnyNumberWhitespace().Replace(text, " ");
+
+			// does the page text contain our search string?
+			if (text.Contains(content, strcomp)) return SearchResult.Found;
+		}
+		return SearchResult.NotFound;
 	}
 
 	/// <summary>
