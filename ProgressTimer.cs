7bc8135852379c36ed81e8901ee9a66b64903c83
John <john@john.com>
2023-07-11 10:45:28 +0100

    Showing sign on durations, and only updating the time if >3.5 seconds or <0.9

--- ProgressTimer.cs
+++ ProgressTimer.cs
@@ -6,11 +6,13 @@ internal sealed class ProgressTimer
 	// Stopwatch is a monotonic timer
 	private readonly Stopwatch watch;
 	private readonly int total;
+	private double lastduration;
 
 	public ProgressTimer(int total)
 	{
 		this.watch = Stopwatch.StartNew();
 		this.total = total;
+		this.lastduration = -1.0;
 	}
 
 	public long Milliseconds => watch.ElapsedMilliseconds;
@@ -25,8 +27,16 @@ internal sealed class ProgressTimer
 
 		var duration = watch.Elapsed.TotalSeconds;           // current duration in seconds
 		var progress = current / (double)total;            // progress as a fraction
-		var expectedduration = duration / progress;        // expected total duration in ms
-		return expectedduration - duration;                      // remaining duration in seconds
+		var expectedduration = duration / progress;        // expected total duration in seconds
+
+		var newexpected = expectedduration - duration;      // remaining time in seconds
+		if (lastduration < 0 || newexpected - lastduration > 3.5 || newexpected < lastduration - 0.9)
+		{
+			// only update if the duration is significantly different (3.5 secs longer that previous calc, or 0.9 secs less)
+			lastduration = newexpected;
+		}
+
+		return lastduration;
 	}
 
 	public long GetRemainingMilliseconds(int current) => (long)(GetRemainingSeconds(current) * 1000.0);
@@ -36,13 +46,15 @@ internal sealed class ProgressTimer
 	/// </summary>
 	public static string SecondsAsText(double sec)
 	{
-		var ts = TimeSpan.FromSeconds(sec);
+		var sign = sec < 0 ? "-" : "";
+		var ts = TimeSpan.FromSeconds(Math.Abs(sec));
+
 		return ts switch
 		{
-			{ TotalDays: >= 1 } => $"{ts.Days} days {ts.Hours} hrs",
-			{ TotalHours: >= 1 } => $"{ts.Hours}:{ts.Minutes:00}:{ts.Seconds:00}",
-			{ TotalMinutes: >= 1 } => $"{ts.Minutes:00}:{ts.Seconds:00}",
-			_ => $"{ts.TotalSeconds:0.0} secs",
+			{ TotalDays: >= 1 } => $"{sign}{ts.Days} days {ts.Hours} hrs",
+			{ TotalHours: >= 1 } => $"{sign}{ts.Hours}:{ts.Minutes:00}:{ts.Seconds:00}",
+			{ TotalMinutes: >= 1 } => $"{sign}{ts.Minutes:00}:{ts.Seconds:00}",
+			_ => $"{sign}{ts.TotalSeconds:0.0} secs",
 		};
 	}
 }
