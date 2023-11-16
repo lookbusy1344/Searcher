13bd4885bfaa624ab052f489d58c4e0018eba943
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-11-16 12:18:38 +0000

    Testing script. Many tests are failing following the .NET 8 update

--- RunTests.cmd
+++ RunTests.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 dotnet build
-vstest.console.exe ".\TestSearcher\bin\Debug\net7.0-windows10.0.22621.0\TestSearcher.dll"
+vstest.console.exe ".\TestSearcher\bin\Debug\net8.0-windows10.0.19041.0\TestSearcher.dll"
