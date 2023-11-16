4fcd18bd464b8d36b5ee52a20ec1c7db359434aa
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-11-16 12:05:52 +0000

    Codefix for a primary constructor

--- SafeCounter.cs
+++ SafeCounter.cs
@@ -3,23 +3,21 @@
 /// <summary>
 /// This is a thread safe Int32 counter using Interlocked.Increment etc
 /// </summary>
-internal sealed class SafeCounter
+/// <remarks>
+/// Create a new counter with an optional start value
+/// </remarks>
+/// <param name="startvalue">The initial value</param>
+internal sealed class SafeCounter(int startvalue = 0)
 {
 	/// <summary>
 	/// The counter, cannot be accessed directly in a thread safe way
 	/// </summary>
-	private int counter;    // this class exists to wrap this field and prevent direct access
+	private int counter = startvalue;    // this class exists to wrap this field and prevent direct access
 
 	/// <summary>
 	/// Get the current value of the counter
 	/// </summary>
 	public int Value => counter;    // no need for Interlocked here, Int32 is atomic on both 32 and 64 bit
-
-	/// <summary>
-	/// Create a new counter with an optional start value
-	/// </summary>
-	/// <param name="startvalue">The initial value</param>
-	public SafeCounter(int startvalue = 0) => counter = startvalue;
 
 	/// <summary>
 	/// Increment the counter in a thread safe way. Lock free
