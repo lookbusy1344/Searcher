58c0d8ece1f8a030d5749570a73753859045dd82
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-08-25 20:27:02 +0100

    Check if token cancelled initially

--- SearcherCli/MainSearch.cs
+++ SearcherCli/MainSearch.cs
@@ -10,48 +10,44 @@ internal sealed class MainSearch : IDisposable
 		// var filesCount = 0;
 		// var modulo = 20;
 
-		var cts = CancellationToken;
+		CancellationToken.Token.ThrowIfCancellationRequested();
 
-		try {
-			if (string.IsNullOrWhiteSpace(config.Search)) {
-				throw new OperationCanceledException();
+		if (string.IsNullOrWhiteSpace(config.Search)) {
+			throw new OperationCanceledException();
+		}
+
+		// outerPatterns are the physical files to find, and may include .zip files even if not given as a pattern, when -z is selected
+		// innerPatterns are for searching inside zip files. May be an empty list for everything, but explicitly doesnt include .zip
+		var innerPatterns = Utils.ProcessInnerPatterns(config.Pattern);
+		var outerPatterns = Utils.ProcessOuterPatterns(config.Pattern, config.InsideZips);
+		if (outerPatterns.Count == 0) {
+			throw new("No pattern specified");
+		}
+
+		// Parallel routine for searching folders
+		//var sw = Stopwatch.StartNew();
+		var files = GlobSearch.ParallelFindFiles(config.Folder.FullName, outerPatterns, parallelThreads, null, CancellationToken.Token);
+		//Debug.WriteLine($"Found {files.Length} files in {sw.ElapsedMilliseconds}ms");
+
+		// Work out a reasonable update frequency for the progress bar
+		// filesCount = files.Length;
+		// modulo = Utils.CalculateModulo(filesCount);
+
+		// var progressTimer = new ProgressTimer(filesCount);
+		// var counter = new SafeCounter();
+
+		// search the files in parallel
+		_ = Parallel.ForEach(files, new() { MaxDegreeOfParallelism = parallelThreads, CancellationToken = CancellationToken.Token }, file => {
+			// Search the file for the search string
+			var found = SearchFile.FileContainsStringWrapper(file, config.Search, innerPatterns, config.StringComparison,
+				CancellationToken.Token);
+			if (found is SearchResult.Found or SearchResult.Error) {
+				ShowResult(new(file, found));
 			}
 
-			// outerPatterns are the physical files to find, and may include .zip files even if not given as a pattern, when -z is selected
-			// innerPatterns are for searching inside zip files. May be an empty list for everything, but explicitly doesnt include .zip
-			var innerPatterns = Utils.ProcessInnerPatterns(config.Pattern);
-			var outerPatterns = Utils.ProcessOuterPatterns(config.Pattern, config.InsideZips);
-			if (outerPatterns.Count == 0) {
-				throw new("No pattern specified");
-			}
-
-			// Parallel routine for searching folders
-			//var sw = Stopwatch.StartNew();
-			var files = GlobSearch.ParallelFindFiles(config.Folder.FullName, outerPatterns, parallelThreads, null, cts.Token);
-			//Debug.WriteLine($"Found {files.Length} files in {sw.ElapsedMilliseconds}ms");
-
-			// Work out a reasonable update frequency for the progress bar
-			// filesCount = files.Length;
-			// modulo = Utils.CalculateModulo(filesCount);
-
-			// var progressTimer = new ProgressTimer(filesCount);
-			// var counter = new SafeCounter();
-
-			// search the files in parallel
-			_ = Parallel.ForEach(files, new() { MaxDegreeOfParallelism = parallelThreads, CancellationToken = cts.Token }, file => {
-				// Search the file for the search string
-				var found = SearchFile.FileContainsStringWrapper(file, config.Search, innerPatterns, config.StringComparison, cts.Token);
-				if (found is SearchResult.Found or SearchResult.Error) {
-					ShowResult(new(file, found));
-				}
-
-				// when the task completes, update completed counter. This is thread safe
-				//var currentCount = counter.Increment();
-			});
-		}
-		catch {
-			// just ignore it
-		}
+			// when the task completes, update completed counter. This is thread safe
+			//var currentCount = counter.Increment();
+		});
 	}
 
 	/// <summary>
