6e2346b017bb9e24af23d1e22267b86dd6ca7746
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-10-26 16:29:17 +0000

    Fixing remaining test

--- TestSearcher/SearcherCorePdfTests.cs
+++ TestSearcher/SearcherCorePdfTests.cs
@@ -67,7 +67,7 @@ public class SearcherCorePdfTests
 
 			var resultCaseSensitive = SearchFile.FileContainsStringWrapper(
 				tempFile,
-				"UPPERCASE",
+				"uppercase",
 				[],
 				StringComparison.Ordinal,
 				CancellationToken.None
